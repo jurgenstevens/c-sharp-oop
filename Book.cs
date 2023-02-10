09d0da2464b496124cb0acedcc67d6ef319e7f25
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-09 22:56:26 -0600

    Create Book class and instantiate two books.

---
+++ Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	public class Book
+	{
+			public string title;
+			public string author;
+			public int pages;
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using OOP;
+using static System.Net.Mime.MediaTypeNames;
 
+namespace ControlFlow
+{
+    class ControlFlow
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "JRR Tolkein";
+            book2.pages = 700;
+
+            Console.ReadLine();
+        }
+    }
+}
